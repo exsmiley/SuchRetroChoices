4d4d051d742ab39f013095ad5a3544a5b483f3f8
Zachery Miranda <exsmiley@gmail.com>
2018-01-19 23:35:16 -0500

    serves index.html

--- main.go
+++ main.go
@@ -7,15 +7,19 @@ import (
     "github.com/gorilla/mux"
 )
 
+// inspiration from:
+// http://www.giantflyingsaucer.com/blog/?p=5635
+
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello World! :O")
+}
+
+
 func main() {
     r := mux.NewRouter()
-    r.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-        // vars := mux.Vars(r)
-        // title := vars["title"]
-        // page := vars["page"]
-
-        fmt.Fprintf(w, "Hello World! :O")
-    })
+    r.Handle("/", http.FileServer(http.Dir("./static/")))
+    r.HandleFunc("/hello", helloHandler)
 
     http.ListenAndServe(":8000", r)
 }
