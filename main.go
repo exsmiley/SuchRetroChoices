c1180c21563e23f9960e4c40f30b4ee113c3d733
Zachery Miranda <exsmiley@gmail.com>
2018-01-20 00:09:02 -0500

    hehe has a chat thingy

--- main.go
+++ main.go
@@ -7,6 +7,28 @@ import (
     "github.com/gorilla/mux"
 )
 
+// specifies either a singular int or list of ints that must be present together
+type Requirement struct {
+    needed []int
+}
+
+// says what options should be given if one of the requirements is met
+type Path struct {
+    options []int
+    requirement []Requirement
+}
+
+type ActionText struct {
+    action string
+    text string
+}
+
+// object with text (id to text of the id) and paths (actions that can result from the specified id)
+type Story struct {
+    actionText map[int]ActionText
+    paths map[int][]Path
+}
+
 // inspiration from:
 // http://www.giantflyingsaucer.com/blog/?p=5635
 
@@ -17,7 +39,13 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 
 
 func main() {
+
     r := mux.NewRouter()
+
+    s := http.StripPrefix("/static/", http.FileServer(http.Dir("./static/")))
+    r.PathPrefix("/static/").Handler(s)
+    // http.Handle("/", r)
+    
     r.Handle("/", http.FileServer(http.Dir("./static/")))
     r.HandleFunc("/hello", helloHandler)
 
